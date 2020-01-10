41e3f0edab848886c22a0dd1ad2db85735a56fac
suzanalupu <56161949+suzanalupu@users.noreply.github.com>
2020-01-10 20:19:02 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 
-/*$servername = "localhost";
+$servername = "localhost";
 $username = "root";
 $password = "";
-$db = "onlineshop";*/
+$db = "onlineshop";
 
 // Create connection
 $con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "online-shopping-server.mysql.database.azure.com", "suzanalupu@online-shopping-server", {your_password}, {your_database}, 3306);
