c91d79c3e82c885c1bf458b36b0051a00dd290fc
suzanalupu <56161949+suzanalupu@users.noreply.github.com>
2020-01-13 21:20:55 +0200

    Update db.php

--- db.php
+++ db.php
@@ -11,10 +11,16 @@ $db = "onlineshop";
 
 // $con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "online-shopping-server.mysql.database.azure.com", "suzanalupu@online-shopping-server", {your_password}, {your_database}, 3306);
 
-$con = mysqli_connect($servername, $username, $password,$db);
+//$con = mysqli_connect($servername, $username, $password,$db);
+
+$con = mysqli_init();
+mysqli_real_connect($con, $servername, $username, $password, $db, 3306);
 // Check connection
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
+//if (!$con) {
+  //  die("Connection failed: " . mysqli_connect_error());
+//}
+if (mysqli_connect_errno($con)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 
